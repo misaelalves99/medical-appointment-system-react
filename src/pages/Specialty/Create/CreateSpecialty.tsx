27d21e04a0f52a769cc1e9059bff98d4bd922937
Misael Alves <112561288+misaelalves99@users.noreply.github.com>
2025-08-27 16:28:51 -0300

    Atualizando redirecionamento

--- src/pages/Specialty/Create/CreateSpecialty.tsx
+++ src/pages/Specialty/Create/CreateSpecialty.tsx
@@ -1,18 +1,21 @@
 // src/pages/Specialty/Create/CreateSpecialty.tsx
 
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import styles from "./CreateSpecialty.module.css";
 import { useSpecialty } from "../../../hooks/useSpecialty";
 
 const CreateSpecialty: React.FC = () => {
   const { addSpecialty } = useSpecialty();
   const [name, setName] = useState("");
+  const navigate = useNavigate();
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
     if (name.trim()) {
       addSpecialty(name.trim());
       setName("");
+      navigate("/specialty"); // redireciona para a página de listagem após criar
     }
   };
 
